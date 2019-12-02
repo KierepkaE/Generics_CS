1b7d0159e95aa7ba555ec1725f698db5455131f2
kierepkae <kierepkaedyta@gmail.com>
2019-12-02 11:51:34 +0000

    create a delegate VideoEncodedEventHandler and event VideoEncodedEventHandler

--- VideoEncoder.cs
+++ VideoEncoder.cs
@@ -4,9 +4,23 @@ using System.Threading;
 namespace generics {
 
   public class VideoEncoder {
+    // To create an event
+    // 1 Define a delegate
+    // 2 Define an event
+    // 3 Raise the event
+
+    public delegate void VideoEncodedEventHandler (object source, EventArgs args);
+    public event VideoEncodedEventHandler VideoEncoded;
+
     public void Encode (Video video) {
       System.Console.WriteLine ("Encoding video in progress . . . ");
       Thread.Sleep (3000);
+      OnVideoEncoded ();
+    }
+
+    protected virtual void OnVideoEncoded () {
+      if (VideoEncoded != null)
+        VideoEncoded (this, EventArgs.empty);
     }
   }
 }
