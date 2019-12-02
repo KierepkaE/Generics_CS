04bcd56c5dac9d35f095a69b11f5405dcfd11500
kierepkae <kierepkaedyta@gmail.com>
2019-12-02 11:42:45 +0000

    create an instance of Video and VideoEncoder

--- Program.cs
+++ Program.cs
@@ -8,14 +8,15 @@ namespace generics {
             // number => number * number
 
             Func<int, int> square = number => number * number;
-
             Console.WriteLine (square (5));
-
             const int factor = 5;
             Func<int, int> multiply = n => n * factor;
-
             var result = multiply (10);
             System.Console.WriteLine (result);
+
+            var video = new Video () { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder ();
+            videoEncoder.Encode (video);
 
         }
     }
